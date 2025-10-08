d1433325dc9ec6f54a3f657fdb9537d5684ae367
s3r442 <s3rzhik23@yandex.ru>
2025-10-08 16:49:38 +0300

    first self import

---
+++ gameparts/parts.py
@@ -0,0 +1,12 @@
+class PlayGround:
+
+    def __init__(self):
+        self.board = [["       " for _ in range(3)] for _ in range(3)]
+
+    def make_move(self, row, col, char):
+        self.board[row][col] = "   " + char + "   "
+
+    def display(self):
+        for row in self.board:
+            print("|".join(row))
+            print("-" * 23)
